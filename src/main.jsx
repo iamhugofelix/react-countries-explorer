d755f9bb8f47ab5ccca064b74fee1a54bc5b7a34
Hugo Felix <hey@uxhugo.com>
2025-07-22 18:38:21 +0100

    Added and created routes to homepage and not found page

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import './styles/reset.scss';
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Rout path='*' element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,4 @@
+export default function Homepage () {
+
+    return <h1>This will be my countries explorer app!</h1>
+}
